267e355791cdd1261ff0196fc4545427675322b1
egollas <egollas@gmail.com>
2023-02-08 14:57:13 -0500

    test

--- index.js
+++ index.js
@@ -5,6 +5,5 @@ const product = require("./api/product");
 app.use(express.json({ extended: false }));
 
 app.use("/api/product", product);
-
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
